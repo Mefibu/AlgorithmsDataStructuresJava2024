de780fb827b874e52ae8f4efc19395b03aff140c
Mefibu <mariia.hyrba@stud.onu.edu.ua>
2024-10-30 21:02:56 +0100

    Solution for LinkedList

---
+++ LinkedList/ListNode.java
@@ -0,0 +1,22 @@
+package LinkedList;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    
+    
+    public ListNode() {
+        this.val = 0;
+        this.next = null;
+    }
+    
+    public ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+    
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
